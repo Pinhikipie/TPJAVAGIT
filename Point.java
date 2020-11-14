811e5b1466555a359ef13ab98178d63d0a99639d
unknown <ngoc-nhi.huynh@dauphine.eu>
2020-11-14 17:36:06 +0100

    Modif du constructeur par défaut de la classe Point

--- Point.java
+++ Point.java
@@ -4,6 +4,11 @@ public class Point {
 	private int abs;
 	private int ord;
 	
+	public Point (){
+		this.abs=10;
+		this.ord=10;
+	}
+	
 	public void translation(int x, int y) {
 		abs += x;
 		ord += y;
